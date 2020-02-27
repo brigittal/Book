9e1cc6a1c65ecb86673e3481b67422d8342a808f
lesia <shod4@yandex.ru>
2020-02-27 14:27:19 +0500

    все что нужно на данном этапе - работает

--- app/src/main/java/com/example/book/MainActivity.java
+++ app/src/main/java/com/example/book/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.book;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import android.content.Intent;
@@ -17,25 +23,35 @@ import android.content.Intent;
 public class MainActivity extends AppCompatActivity
 {
     List<Word> words = new ArrayList<>();
+    RecyclerView recyclerView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setInitialData();
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         DataAdapter adapter = new DataAdapter(this, words);
         recyclerView.setAdapter(adapter);
     }
 
-    private void setInitialData()
-    {
-        words.add(new Word ("Кошка", "Cat"));
-        words.add(new Word ("Собака", "Dog"));
-        words.add(new Word ("Дом", "House"));
-        words.add(new Word ("Свинья", "Pig"));
-    }
+        private void setInitialData()
+        {
+
+            words.add(new Word ("Кошка ", "Cat"));
+            words.add(new Word ("Собака ", "Dog"));
+            words.add(new Word ("Дом ", "House"));
+            words.add(new Word ("Свинья ", "Pig"));
+            words.add(new Word ("Кошка ", "Cat"));
+            words.add(new Word ("Собака ", "Dog"));
+            words.add(new Word ("Дом ", "House"));
+            words.add(new Word ("Свинья ", "Pig"));
+            words.add(new Word ("Кошка ", "Cat"));
+            words.add(new Word ("Собака ", "Dog"));
+            words.add(new Word ("Дом ", "House"));
+            words.add(new Word ("Свинья ", "Pig"));
+        }
 
 
     @Override
@@ -48,11 +64,27 @@ public class MainActivity extends AppCompatActivity
         if (item.getItemId() == R.id.menu_item) {
             Intent intent = new Intent(this, add.class);
             startActivity(intent);
-
-
         }
-
-
     }
+
+    /*public String readData(String fileName){
+       try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(openFileInput(fileName)));
+
+            String line = "";
+            While((line = reader.readLine())!=null){
+                return line;
+            };
+
+        } catch (FileNotFoundException ex){
+            ex.printStackTrace();
+        }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        return null;
+    }*/
+
+
 
 }
--- app/src/main/java/com/example/book/add.java
+++ app/src/main/java/com/example/book/add.java
@@ -1,14 +1,57 @@
 package com.example.book;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+
 
 public class add extends AppCompatActivity {
+
+    Button addbut;
+    EditText word;
+    EditText trword;
+    final static String fileName = "test";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        word = (EditText) findViewById(R.id.word);
+        trword = (EditText) findViewById(R.id.trword);
+        addbut = (Button) findViewById(R.id.addbut);
+        addbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData(fileName,word.getText().toString() + " " + trword.getText().toString() );
+                Log.d("LOG", "Файл записан");
+
+            }
+        });
+    }
+
+    // сохранение файла
+    public void saveData(String fileName, String data) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(openFileOutput(fileName, MODE_APPEND)));
+            writer.write(data);
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+                e.printStackTrace();
+        }
     }
 }
